f1f201231c1f0dcc8fd4d2033341dd4213c8fef7
Alan Krajina <alankrajina88@gmail.com>
2019-10-18 12:34:17 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,19 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+export default class DelayedButton extends React.Component {
+
+  handleFocus = () => {
+    console.log('Good!');
+  }
+
+  handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  }
+
+  render() {
+    return (
+      <button onFocus={this.handleFocus} onBlur={this.handleBlur}></button>
+    );
+  }
+}
